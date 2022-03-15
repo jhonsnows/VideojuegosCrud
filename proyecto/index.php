1923446a5086c734b0f4a398a3026fc9cb086cd0
Diego <diancaladinamus@gmail.com>
2022-03-15 10:03:31 -0500

    Update index.php
    Buscar por ID agregado

--- proyecto/index.php
+++ proyecto/index.php
@@ -23,12 +23,63 @@
 
             <br>
         </div>
-            <div class="formulario">
-<h4>Buscar Videojuego por ID</h4>
-            <form action="submit">
-                <input type="number" class="caja1" placeholder="Realice la búsqueda de un juego por ID">
-                <button type="submit">buscar</button>
+        <div class="formulario">
+
+        <!-- Inicia searchbox juego por ID-->
+
+        <h4>Buscar Videouego por ID</h4>
+            <p><i>Id de prueba: 2028</i></p>
+            <form action="" method="GET">
+
+                <input type="text" name="id" value="<?php if (isset($_GET['id'])) {
+                                                        echo $_GET['id'];
+                                                    } ?>">
+
+                <input type="submit" value="Buscar">
+
             </form>
+
+            <hr>
+            
+            <?php
+            $con = mysqli_connect("localhost", "root", "unad2021", "crud");
+
+            if (isset($_GET['id'])) {
+                $id = $_GET['id'];
+
+                $query = "SELECT * FROM games WHERE id='$id' ";
+                $query_run = mysqli_query($con, $query);
+
+                // Motrar la consulta en una tabla
+
+                echo "<table>";
+                echo "<thead>";
+                echo "<tr>";
+                echo "<th>ID</th>";
+                echo "<th>Nombre</th>";
+                echo "<th>Género</th>";
+                echo "<th>Tipo de Consola</th>";
+                echo "<th>Año de Lanzamiento</th>";
+                echo "<th>Desarrollador</th>";
+                echo "</tr>";
+                echo "</thead>";
+
+                // Bucle while que recorre cada registro y muestra cada campo en la tabla.
+                while ($col = mysqli_fetch_array($query_run)) {
+                    echo "<tbody>";
+                    echo "<tr>";
+                    echo "<td>" . $col['id'] . "</td><td>" . $col['name'] . "</td><td>" . $col['genero'] . "</td><td>" . $col['console']  . "</td><td>" . $col['añolanzamiento'] . "</td><td>" . $col['desarrollador'] . "</td>";
+                    echo "</tr>";
+                    echo "</tbody>";
+                }
+
+                echo "</table>";
+            }
+
+            // cerrar conexión de base de datos
+            mysqli_close($con);
+
+            ?>
 
         </div>
         <footer class="pie">
