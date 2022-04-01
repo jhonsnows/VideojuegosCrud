4a4a2d78ee52b4685e7465bb7044544764bba8e7
Diego <diancaladinamus@gmail.com>
2022-04-01 09:57:02 -0500

    Comentar el codigo

--- proyecto/delete.php
+++ proyecto/delete.php
@@ -8,9 +8,13 @@ $password = "unad2021";
 
 $conexion = mysqli_connect($servername, $username, $password, $database);
 
+//Variable "ID" enviada por metodo post al presionar el boton Eliminar.
+//Se hace la consulta en la base de datos y se elimina la fila indicada en el valor "id"
 
 $ID = $_POST['id'];
 mysqli_query($conexion, "DELETE FROM games where id='$ID'") or die("Error al eliminar");
 
+//cerrar conexion
 mysqli_close($conexion);
+//redirigir a pagina "leer.php"
 header("location:leer.php");
